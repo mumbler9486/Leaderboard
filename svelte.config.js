be4ce5857e00658d64763bc9186dafd3c83dc8e7
SliWhist <122509026+SliWhist@users.noreply.github.com>
2023-03-05 03:25:18 -0500

    routing fight

--- svelte.config.js
+++ svelte.config.js
@@ -5,24 +5,24 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	kit: {
 		adapter: azure({
-			"auth": {
-			  "rolesSource": "/ngs-api/LoginGetRolesPSO2",
-			  "identityProviders": {
-				"customOpenIdConnectProviders": {
-				  "aadb2c": {
-					"registration": {
-					  "clientIdSettingName": "AADB2C_PROVIDER_CLIENT_ID",
-					  "clientCredential": {
-						"clientSecretSettingName": "AADB2C_PROVIDER_CLIENT_SECRET"
+			auth: {
+			  rolesSource: "/ngs-api/LoginGetRolesPSO2",
+			  identityProviders: {
+				customOpenIdConnectProviders: {
+				  aadb2c: {
+					registration: {
+					  clientIdSettingName: "AADB2C_PROVIDER_CLIENT_ID",
+					  clientCredential: {
+						clientSecretSettingName: "AADB2C_PROVIDER_CLIENT_SECRET"
 					  },
-					  "openIdConnectConfiguration": {
-						"wellKnownOpenIdConfiguration": "https://pso2centralb2c.b2clogin.com/pso2centralb2c.onmicrosoft.com/B2C_1_SignUpSignInFlow/v2.0/.well-known/openid-configuration"
+					  openIdConnectConfiguration: {
+						wellKnownOpenIdConfiguration: "https://pso2centralb2c.b2clogin.com/pso2centralb2c.onmicrosoft.com/B2C_1_SignUpSignInFlow/v2.0/.well-known/openid-configuration"
 					  }
 					},
-					"login": {
-					  "nameClaimType": "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
-					  "scopes": ["openid","profile","email"],
-					  "loginParameterNames": []
+					login: {
+					  nameClaimType: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
+					  scopes: ["openid","profile","email"],
+					  loginParameterNames: []
 					}
 				  }
 				}
@@ -31,64 +31,64 @@ const config = {
 			trailingSlash: "never",
 			routes: [
 			  {
-				"route": "/profile*",
-				"allowedRoles": ["user"],
+				route: "/profile",
+				allowedRoles: ["user"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/admin*",
-				"allowedRoles": ["administrator"],
+				route: "/admin*",
+				allowedRoles: ["administrator"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/moderator*",
-				"allowedRoles": ["administrator","moderator"],
+				route: "/moderator*",
+				allowedRoles: ["administrator","moderator"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/ngs-api/ApproveRun*",
-				"allowedRoles": ["administrator","moderator"],
+				route: "/ngs-api/ApproveRun*",
+				allowedRoles: ["administrator","moderator"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/ngs-api/DenyRun*",
-				"allowedRoles": ["administrator","moderator"],
+				route: "/ngs-api/DenyRun*",
+				allowedRoles: ["administrator","moderator"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/ngs-api/UpdateProfile*",
-				"allowedRoles": ["user"],
+				route: "/ngs-api/UpdateProfile*",
+				allowedRoles: ["user"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/ngs-api/SubmitRun*",
-				"allowedRoles": ["user"],
+				route: "/ngs-api/SubmitRun*",
+				allowedRoles: ["user"],
 				rewrite: "/api/__render"
 			  },
 			  {
-				"route": "/submit*",
-				"allowedRoles": ["user"],
+				route: "/submit",
+				allowedRoles: ["user"],
 				rewrite: "/api/__render"
 			  },
 			  {
 				route: "/login",
-				rewrite: "/.auth/login/aadb2c"
+				redirect: "/.auth/login/aadb2c"
 			  },
 			  {
-				"route": "/.auth/login/aad",
-				"statusCode": 404
+				route: "/.auth/login/aad",
+				statusCode: 404
 			  },
 			  {
-				"route": "/.auth/login/github",
-				"statusCode": 404
+				route: "/.auth/login/github",
+				statusCode: 404
 			  },
 			  {
-				"route": "/.auth/login/twitter",
-				"statusCode": 404
+				route: "/.auth/login/twitter",
+				statusCode: 404
 			  },
 			  {
-				"route": "/logout",
-				"redirect": "/.auth/logout"
+				route: "/logout",
+				redirect: "/.auth/logout"
 			  }
 			]
 		  })
