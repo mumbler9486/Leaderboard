03c962a3af17afab0ffbe1f6f3bf09f8144492db
mumbler9486 <mumbler9486@gmail.com>
2023-05-01 08:29:51 +0900

    🚚 Rename mock files

--- src/mocks/inject.ts
+++ src/mocks/inject.ts
@@ -7,12 +7,12 @@ import { browser, dev } from '$app/environment';
 export async function inject() {
 	if (dev && browser && import.meta.env.VITE_MSW_SERVER_ONLY !== 'true') {
 		console.log('Client MSW started');
-		const { worker } = await import('./client/worker');
+		const { worker } = await import('./client/setup');
 		return worker.start({ onUnhandledRequest: 'bypass' }).catch(console.warn);
 	}
 	if (dev && !browser) {
 		console.log('Server MSW started');
-		const { server } = await import('./server/server');
+		const { server } = await import('./server/setup');
 		return server.listen({ onUnhandledRequest: 'bypass' });
 	}
 }
--- src/mocks/server/setup.ts
+++ src/mocks/server/setup.ts
@@ -1,4 +1,4 @@
 import { setupServer } from 'msw/node';
-import { serverHandlers } from './handlers.server';
+import { serverHandlers } from './handlers';
 
 export const server = setupServer(...serverHandlers);
