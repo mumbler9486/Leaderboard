93bbf5d4ed5360ba8710831c947183a7526fa518
mumbler9486 <mumbler9486@gmail.com>
2023-05-01 07:50:45 +0900

    🤡 Update SWA launch to server only mock

--- src/mocks/inject.ts
+++ src/mocks/inject.ts
@@ -5,11 +5,13 @@ import { browser, dev } from '$app/environment';
  * build/runtime due to invalid imports from server/client.
  */
 export async function inject() {
-	if (dev && browser) {
+	if (dev && browser && import.meta.env.VITE_MSW_SERVER_ONLY !== 'true') {
+		console.log('Client MSW started');
 		const { worker } = await import('./client/worker');
 		return worker.start({ onUnhandledRequest: 'bypass' }).catch(console.warn);
 	}
 	if (dev && !browser) {
+		console.log('Server MSW started');
 		const { server } = await import('./server/server');
 		return server.listen({ onUnhandledRequest: 'bypass' });
 	}
