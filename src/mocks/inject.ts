c5ad42f2193c50bc96ce5554d46d23c0b0d5e4f4
mumbler9486 <mumbler9486@gmail.com>
2023-05-03 14:59:33 +0900

    🔥Remove MSW server mock for now

--- src/mocks/inject.ts
+++ src/mocks/inject.ts
@@ -5,14 +5,9 @@ import { browser, dev } from '$app/environment';
  * build/runtime due to invalid imports from server/client.
  */
 export async function inject() {
-	if (dev && browser && import.meta.env.VITE_MSW_SERVER_ONLY !== 'true') {
+	if (dev && browser) {
 		console.log('Client MSW started');
 		const { worker } = await import('./client/setup');
 		return worker.start({ onUnhandledRequest: 'bypass' }).catch(console.warn);
-	}
-	if (dev && !browser) {
-		console.log('Server MSW started');
-		const { server } = await import('./server/setup');
-		return server.listen({ onUnhandledRequest: 'bypass' });
 	}
 }
--- src/mocks/server/setup.ts
+++
@@ -1,4 +0,0 @@
-import { setupServer } from 'msw/node';
-import { serverHandlers } from './handlers';
-
-export const server = setupServer(...serverHandlers);
