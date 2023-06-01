170797626576f78a96aac37c3afde6e172ef4bea
mumbler9486 <mumbler9486@gmail.com>
2023-06-01 21:09:09 +0900

    💬 Change patch code to level 75

--- src/lib/constants/patchCodes.ts
+++ src/lib/constants/patchCodes.ts
@@ -1,10 +1,10 @@
 // The current patch code for all submissions
-export const CurrentSubmissionPatchCode = '2023jun7rebalance';
+export const CurrentSubmissionPatchCode = 'level75r';
 
 // Keep patch codes under 30 chars
 export const patchCodeLabelMap: { [id: string]: string } = {
 	['60r']: '+60 Release',
 	['slayer']: 'Slayer Release',
 	['pot6r']: 'Potential Lv6. Release',
-	['jun7rebalance']: 'June 7 Rebalance'
+	['level75r']: 'Level 75 Release'
 };
