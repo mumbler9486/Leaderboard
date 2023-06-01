48d874e6db3e42d1632c01d6c7de5b3aed899efb
mumbler9486 <mumbler9486@gmail.com>
2023-06-01 19:22:02 +0900

    💬 Update patch to jun 7 rebalance

--- src/lib/constants/patchCodes.ts
+++ src/lib/constants/patchCodes.ts
@@ -1,8 +1,10 @@
 // The current patch code for all submissions
-export const CurrentSubmissionPatchCode = 'pot6r';
+export const CurrentSubmissionPatchCode = '2023jun7rebalance';
 
+// Keep patch codes under 30 chars
 export const patchCodeLabelMap: { [id: string]: string } = {
 	['60r']: '+60 Release',
 	['slayer']: 'Slayer Release',
-	['pot6r']: 'Potential Lv6. Release'
+	['pot6r']: 'Potential Lv6. Release',
+	['jun7rebalance']: 'June 7 Rebalance'
 };
