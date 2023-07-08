ed4b9aa0c0af060992a8df260952a9198fb6eec9
mumbler9486 <mumbler9486@gmail.com>
2023-07-08 22:04:26 +0900

    🐛 Fix incorrect identity path

--- src/routes/ngs-api/auth/getUserRoles/+server.ts
+++ src/routes/ngs-api/auth/getUserRoles/+server.ts
@@ -7,8 +7,8 @@ import { jsonError } from '$lib/server/error.js';
 // For b2c
 export async function POST({ request }) {
 	const b2cIdentity = await request.json();
-	const userGuid = b2cIdentity.userId;
-	console.log('B2C TEST IDENITY THINGY', b2cIdentity);
+	const userGuid = b2cIdentity.objectId;
+
 	if (!guidRegex.test(userGuid)) {
 		console.error(`Malformed B2C Request. objectId not a Guid.`);
 		return jsonError(400, 'Invalid objectId. Not a Guid');
