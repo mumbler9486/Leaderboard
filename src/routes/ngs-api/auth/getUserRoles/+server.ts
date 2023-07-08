d842070156e4fb1a04bd707948fd0fd1e945205f
mumbler9486 <mumbler9486@gmail.com>
2023-07-08 21:58:36 +0900

    🐛 Fix login issue not getting roles

--- src/routes/ngs-api/auth/getUserRoles/+server.ts
+++ src/routes/ngs-api/auth/getUserRoles/+server.ts
@@ -1,15 +1,26 @@
 import { json } from '@sveltejs/kit';
 import { leaderboardDb } from '$lib/server/db/db.js';
-import { getUserRoles } from '$lib/server/repositories/userRepository.js';
+import { getUserRoles } from '$lib/server/repositories/userRepository.ts';
+import { guidRegex } from '$lib/utils/validation.js';
+import { jsonError } from '$lib/server/error.js';
 
 // For b2c
 export async function POST({ request }) {
+	const b2cIdentity = await request.json();
+	const userGuid = b2cIdentity.userId;
+	console.log('B2C TEST IDENITY THINGY', b2cIdentity);
+	if (!guidRegex.test(userGuid)) {
+		console.error(`Malformed B2C Request. objectId not a Guid.`);
+		return jsonError(400, 'Invalid objectId. Not a Guid');
+	}
+
 	try {
 		const pool = await leaderboardDb.connect();
-		const userGuid = await request.text();
 		const userRoles = await getUserRoles(await pool.request(), userGuid);
 
-		return json(userRoles);
+		return json({
+			roles: userRoles
+		});
 	} catch (err) {
 		console.error(err);
 	}
