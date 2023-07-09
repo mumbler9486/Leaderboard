520270416c2282f79bcae066ce3a2f8448b0740f
mumbler9486 <mumbler9486@gmail.com>
2023-07-10 08:52:58 +0900

    ♻️ Refactor error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,32 +1,29 @@
 <script lang="ts">
 	import { t } from 'svelte-i18n';
-	// @ts-nocheck
 	import { page } from '$app/stores';
-	import FOFError from '$lib/PageComponents/Error/FOFError.svelte';
-	import AuthError from '$lib/PageComponents/Error/AuthError.svelte';
+	import ErrorSplash from '$lib/PageComponents/Error/ErrorSplash.svelte';
+
+	$: pageStatus = $page.status;
+	$: pageErrorMessage = $page.error?.message ?? 'Error';
 </script>
 
 <svelte:head>
-	{#if $page.status == 404}
+	{#if pageStatus == 404}
 		<title>{$t('shared.siteName')} - 404 Not Found</title>
-	{:else if $page.status == 401}
+	{:else if pageStatus == 401}
 		<title>{$t('shared.siteName')} - 401 Not Authorized</title>
 	{:else}
-		<title>{$t('shared.siteName')} - {$page.status} {$page.error.message}</title>
+		<title>{$t('shared.siteName')} - {pageStatus} {pageErrorMessage}</title>
 	{/if}
 </svelte:head>
 <div class="flex grow flex-col content-center">
-	<div class="container m-16 mx-auto flex grow rounded-md border border-secondary bg-base-100/75">
-		<div
-			class="m-2 flex grow flex-col rounded-md border border-secondary bg-base-100 p-4 px-8 md:flex-row"
-		>
-			{#if $page.status == 404}
-				<FOFError />
-			{:else if $page.status == 401}
-				<AuthError />
-			{:else}
-				<h1>{$page.status} {$page.error.message}</h1>
-			{/if}
-		</div>
+	<div class="container mx-auto mt-8 flex grow rounded-md border border-secondary bg-base-100/75">
+		{#if pageStatus == 404}
+			<ErrorSplash status={404} message={'Page not found'} />
+		{:else if pageStatus == 401}
+			<ErrorSplash status={401} message={'Not Authorized'} />
+		{:else}
+			<h1>{pageStatus} {pageErrorMessage}</h1>
+		{/if}
 	</div>
 </div>
