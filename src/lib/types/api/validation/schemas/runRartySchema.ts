f186ad347d71d4f74795d11b53d7a5c07c1effa3
mumbler9486 <mumbler9486@gmail.com>
2023-08-05 22:59:39 +0900

    ♻️ Refactor run party yup schema

---
+++ src/lib/types/api/validation/schemas/runRartySchema.ts
@@ -0,0 +1,70 @@
+import { type InferType, string, number, object, array, mixed } from 'yup';
+import { NgsPlayerClass } from '../../ngsPlayerClass';
+import { Weapon } from '../../weapon';
+import { youtubeUrlRegex } from '$lib/utils/youtube';
+
+const mainClasses = [
+	NgsPlayerClass.Hunter,
+	NgsPlayerClass.Fighter,
+	NgsPlayerClass.Ranger,
+	NgsPlayerClass.Gunner,
+	NgsPlayerClass.Force,
+	NgsPlayerClass.Techter,
+	NgsPlayerClass.Braver,
+	NgsPlayerClass.Bouncer,
+	NgsPlayerClass.Waker,
+	NgsPlayerClass.Slayer
+];
+const subClasses = mainClasses;
+
+const weapons = [
+	Weapon.Sword,
+	Weapon.WiredLance,
+	Weapon.Partisan,
+	Weapon.TwinDaggers,
+	Weapon.DoubleSabers,
+	Weapon.Knuckles,
+	Weapon.Katana,
+	Weapon.SoaringBlades,
+	Weapon.Gunblade,
+	Weapon.AssaultRifle,
+	Weapon.Launcher,
+	Weapon.TwinMachineGuns,
+	Weapon.Bow,
+	Weapon.Rod,
+	Weapon.Talis,
+	Weapon.Wand,
+	Weapon.JetBoots,
+	Weapon.Harmonizer
+];
+
+export const yupRunPartySchema = (maxPlayers: number = 4) => {
+	return array(
+		object({
+			playerId: number(),
+			ordinal: number().default(0).min(0).max(maxPlayers),
+			povLink: string()
+				.matches(youtubeUrlRegex, (w) => `${w.path} must a valid youtube link`)
+				.nullable()
+				.max(100),
+			inVideoName: string().required().max(30),
+			mainClass: mixed<NgsPlayerClass>().required().oneOf(mainClasses),
+			subClass: mixed<NgsPlayerClass>().required().oneOf(subClasses),
+			weapons: array(mixed<Weapon>().required().oneOf(weapons)).max(6).ensure().required()
+		})
+	)
+		.min(1)
+		.max(maxPlayers)
+		.test('has_video', 'At least one player must have a video', (players) =>
+			players?.some((p) => p.povLink !== undefined)
+		)
+		.test(
+			'player1_has_id',
+			'Player 1 must be an existing user',
+			(players) => players?.at(0)?.playerId !== undefined
+		)
+		.test('solo_requires_weapon', 'Solo requires at least 1 weapon used', (players) =>
+			players?.length == 1 ? players?.at(0)?.weapons[0] !== undefined : true
+		)
+		.required();
+};
--- src/lib/types/api/validation/venogiaSubmission.ts
+++ src/lib/types/api/validation/venogiaSubmission.ts
@@ -1,6 +1,7 @@
 import { type InferType, string, number, object, array, mixed, ObjectSchema } from 'yup';
 import { runSubmissionRequestSchema, type RunSubmissionRequest } from './runSubmission';
 import { yupRunTime } from './schemas/timeSchema';
+import { yupRunPartySchema } from './schemas/runRartySchema';
 
 const quest = ['venogia'];
 const categories = ['urgent_quest'];
@@ -12,6 +13,7 @@ export const venogiaRunSubmissionSchema: ObjectSchema<RunSubmissionRequest> =
 		quest: string().required().oneOf(quest),
 		rank: number().oneOf(ranks).required(),
 		category: string().required().oneOf(categories),
+		party: yupRunPartySchema(8),
 		time: yupRunTime(3600)
 	});
 
