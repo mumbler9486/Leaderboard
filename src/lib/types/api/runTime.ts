0c0b035aa1e6510f5d7346672b0c37277762c820
mumbler9486 <mumbler9486@gmail.com>
2023-08-16 08:25:13 +0900

    🐛 Improve ranking labeling for same time ranks

--- src/lib/types/api/runTime.ts
+++ src/lib/types/api/runTime.ts
@@ -3,3 +3,7 @@ export type RunTime = {
 	minutes: number;
 	seconds: number;
 };
+
+export const runTimeEqual = (t1: RunTime, t2: RunTime): boolean => {
+	return t1.hours === t2.hours && t1.minutes === t2.minutes && t1.seconds === t2.seconds;
+};
