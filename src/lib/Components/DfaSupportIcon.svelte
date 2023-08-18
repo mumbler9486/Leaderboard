81133d9bd5a6bec8924d1c6b63885d7950b6f59a
mumbler9486 <mumbler9486@gmail.com>
2023-08-18 20:42:19 +0900

    ♻️Refactor icon to use dfAegis support enum

--- src/lib/Components/DfaSupportIcon.svelte
+++ src/lib/Components/DfaSupportIcon.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+	import { DfAegisSupport } from '$lib/types/api/dfAegis/dfAegisSupports';
+
 	export let support: string;
 
 	const supportMap: { [support: string]: { label: string; icon: string } } = {
-		ilma: { label: 'Ilma', icon: 'ilma.png' },
-		ainamanon: { label: 'Aina & Manon', icon: 'ainamanon.png' },
-		'aina-manon': { label: 'Aina & Manon', icon: 'ainamanon.png' }, //TODO replace DB values
-		aina_manon: { label: 'Aina & Manon', icon: 'ainamanon.png' }, //TODO replace DB values
-		nadereh: { label: 'Nadereh', icon: 'nadereh.png' },
-		glen: { label: 'Glen', icon: 'glen.png' }
+		[DfAegisSupport.Ilma]: { label: 'Ilma', icon: 'ilma.png' },
+		[DfAegisSupport.AinaManon]: { label: 'Aina & Manon', icon: 'ainamanon.png' }, //TODO replace DB values
+		[DfAegisSupport.Nadereh]: { label: 'Nadereh', icon: 'nadereh.png' },
+		[DfAegisSupport.Glen]: { label: 'Glen', icon: 'glen.png' }
 	};
 
 	$: supportInfo = supportMap[support];
