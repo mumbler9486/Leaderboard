04815a91950a7c8aa04e86f629bf3919102bccb4
mumbler9486 <mumbler9486@gmail.com>
2023-08-19 12:37:20 +0900

    🏷️ Refactor filter tag with enums

--- src/lib/Components/Filters/FilterTags/DfAegisSupportFilterTag.svelte
+++ src/lib/Components/Filters/FilterTags/DfAegisSupportFilterTag.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
+	import { DfAegisSupport } from '$lib/types/api/dfAegis/dfAegisSupports';
 	import FilterTag from './FilterTag.svelte';
 
 	export let support: string;
 
 	$: supportInfo = supportMap[support];
 
-	const supportMap: { [support: string]: { label: string; icon: string } } = {
-		ilma: { label: 'Ilma', icon: '/icons/dfaegis/ilma.png' },
-		ainamanon: { label: 'Aina & Manon', icon: '/icons/dfaegis/ainamanon.png' },
-		aina_manon: { label: 'Aina & Manon', icon: '/icons/dfaegis/ainamanon.png' },
-		'aina-manon': { label: 'Aina & Manon', icon: '/icons/dfaegis/ainamanon.png' }, //TODO replace DB values
-		nadereh: { label: 'Nadereh', icon: '/icons/dfaegis/nadereh.png' },
-		glen: { label: 'Glen', icon: '/icons/dfaegis/glen.png' }
+	const supportMap: Record<string, { label: string; icon: string }> = {
+		[DfAegisSupport.Ilma]: { label: 'Ilma', icon: '/icons/dfaegis/ilma.png' },
+		[DfAegisSupport.AinaManon]: { label: 'Aina & Manon', icon: '/icons/dfaegis/ainamanon.png' },
+		[DfAegisSupport.Nadereh]: { label: 'Nadereh', icon: '/icons/dfaegis/nadereh.png' },
+		[DfAegisSupport.Glen]: { label: 'Glen', icon: '/icons/dfaegis/glen.png' }
 	};
 </script>
 
