bb4125cc8e5ca6e2a3fb5afb05c1b37129936ccc
mumbler9486 <mumbler9486@gmail.com>
2023-08-21 22:02:28 +0900

    ♻️ Refactor guid validation

--- src/lib/types/api/validation/runSubmission.ts
+++ src/lib/types/api/validation/runSubmission.ts
@@ -1,19 +1,19 @@
 import { type InferType, string, number, object, array, mixed } from 'yup';
 import { ServerRegion } from '../serverRegions';
-import { guidRegex } from '$lib/utils/validation';
 import { CurrentSubmissionPatchCode } from '$lib/constants/patchCodes';
 import { yupRunTime } from './schemas/timeSchema';
 import { yupRunPartySchema } from './schemas/runPartySchema';
 import type { NgsQuests } from '../runs/quests';
 import type { NgsRunCategories } from '../runs/categories';
 import { yupQuestRank } from './schemas/questRankSchema';
+import { yupGuid } from './schemas/guid';
 
 const serverRegions = [ServerRegion.Global, ServerRegion.Japan];
 const patches = [CurrentSubmissionPatchCode];
 
 // This is only a base schema, inherit and override the fields as necessary
 export const runSubmissionRequestSchema = object({
-	submitterUserId: string().required().matches(guidRegex, 'Not a GUID'),
+	submitterUserId: yupGuid().required(),
 	submitterName: string().required(),
 	quest: mixed<NgsQuests>().required(),
 	category: mixed<NgsRunCategories>().required(),
