f2e9e01639f69006e7ca93d3c00f8cae375dddb6
mumbler9486 <mumbler9486@gmail.com>
2023-08-27 21:36:55 +0900

    ♻️ Refactor server region tag to use enums

--- src/lib/Components/Filters/FilterTags/ServerRegionFilterTag.svelte
+++ src/lib/Components/Filters/FilterTags/ServerRegionFilterTag.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { ServerRegion } from '$lib/types/api/serverRegions';
 	import FilterTag from './FilterTag.svelte';
 
 	export let server: string;
@@ -6,9 +7,9 @@
 	$: regionInfo = regionMap[server.toLowerCase()] ?? regionMap['unknown'];
 
 	const regionMap: { [region: string]: { label: string; icon: string } } = {
-		['global']: { label: 'Global Only', icon: '/icons/server/server-global.png' },
-		['japan']: { label: 'Japan Only', icon: '/icons/server/server-japan.png' },
-		['unknown']: { label: '-', icon: '/icons/class/class-unknown.png' }
+		[ServerRegion.Global]: { label: 'Global Only', icon: '/icons/server/server-global.png' },
+		[ServerRegion.Japan]: { label: 'Japan Only', icon: '/icons/server/server-japan.png' },
+		[ServerRegion.Unknown]: { label: '-', icon: '/icons/class/class-unknown.png' }
 	};
 </script>
 
