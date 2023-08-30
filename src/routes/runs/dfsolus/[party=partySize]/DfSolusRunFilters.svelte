ec64c0e75aa37ff01a450d9a0ab919174f52ddb7
mumbler9486 <mumbler9486@gmail.com>
2023-08-30 20:53:51 +0900

    ♻️ Refactor df solus filter modal

--- src/routes/runs/dfsolus/[party=partySize]/DfSolusRunFilters.svelte
+++ src/routes/runs/dfsolus/[party=partySize]/DfSolusRunFilters.svelte
@@ -4,9 +4,11 @@
 	import ServerRegionFilterTag from '$lib/Components/Filters/FilterTags/ServerRegionFilterTag.svelte';
 	import PartySizeNavigation from '$lib/Components/PartySizeNavigation.svelte';
 	import { parseNgsPlayerClass } from '$lib/types/api/ngsPlayerClass';
+	import RunFilterModal from '../../RunFilterModal.svelte';
+	import RunFilterTags from '../../RunFilterTags.svelte';
+	import RunRules from '../../RunRules.svelte';
 	import DfAegisRules from '../DfSolusRules.svelte';
 	import { runFilters, type DfSolusSearchFilters } from '../dfSolusRunFilterStore';
-	import DfSolusModalRunFilters from './DfSolusModalRunFilters.svelte';
 
 	export let solo: boolean;
 
@@ -23,6 +25,7 @@
 		{ link: '/runs/dfsolus/duo', label: 'Duo' },
 		{ link: '/runs/dfsolus/party', label: 'Party' }
 	];
+	const rules = ['Do not abuse bugs or exploits.'];
 
 	const applyFilters = () => {
 		runFilters.set({ ...filters });
@@ -31,23 +34,13 @@
 	runFilters.subscribe((f) => {
 		filters = { ...f };
 	});
-
-	const resetClassFilter = () => {
-		filters.class = 'no_filter';
-		applyFilters();
-	};
-
-	const resetServerRegion = () => {
-		filters.server = 'no_filter';
-		applyFilters();
-	};
 </script>
 
 <div
 	class="-mx-6 flex grow flex-col rounded-md border border-secondary bg-secondary/25 p-1 md:mx-0"
 >
 	<div class="flex flex-row flex-wrap place-content-center items-stretch gap-2">
-		<div class="grid w-full grid-cols-3 gap-4 py-2 px-4">
+		<div class="grid w-full grid-cols-3 gap-4 px-4 py-2">
 			<PartySizeNavigation parties={partyLinks} />
 		</div>
 	</div>
@@ -55,26 +48,21 @@
 	<Divider class="-mx-1 my-0" />
 	<div class="flex flex-row flex-wrap place-content-center items-stretch">
 		<div class="m-1 md:flex-1">
-			<DfSolusModalRunFilters
+			<RunFilterModal
+				classFilter={solo}
 				bind:server={filters.server}
 				bind:mainClass={filters.class}
 				on:applyFilters={applyFilters}
-				{solo}
 			/>
 		</div>
 		<div class="m-1 md:flex-initial">
-			<DfAegisRules />
+			<RunRules {rules} />
 		</div>
 	</div>
 
 	<Divider class="-mx-1 my-0" />
 
 	<div class="flex flex-row gap-2 px-1">
-		{#if !!playerClassFilterTag}
-			<NgsClassFilterTag ngsClass={playerClassFilterTag} on:click={resetClassFilter} />
-		{/if}
-		{#if $runFilters.server && $runFilters.server != 'no_filter'}
-			<ServerRegionFilterTag server={$runFilters.server} on:click={resetServerRegion} />
-		{/if}
+		<RunFilterTags />
 	</div>
 </div>
