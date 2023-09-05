c888212aef91927d494102bea9d73497b6a33584
mumbler9486 <mumbler9486@gmail.com>
2023-09-05 22:12:05 +0900

    🤡 Fix user mock

--- src/mocks/client/users.mock.ts
+++ src/mocks/client/users.mock.ts
@@ -1,4 +1,5 @@
 import type { PlayerProfile } from '$lib/types/api/players/player';
+import { ServerRegion } from '$lib/types/api/serverRegions';
 
 export const mockPlayer: PlayerProfile = {
 	playerId: 123,
@@ -7,7 +8,7 @@ export const mockPlayer: PlayerProfile = {
 	flag: undefined,
 	characterName: 'Mock',
 	preferredName: 0,
-	server: 'global',
+	server: ServerRegion.Global,
 	nameType: 1,
 	nameColor1: 'ff0000',
 	nameColor2: '00ffff',
