7b17b7ab36d469964a32c100ee4e0f959f84c098
mumbler9486 <mumbler9486@gmail.com>
2023-09-30 13:08:57 +0900

    💬 Add tooltip about different patch selection

--- src/routes/submit/CurrentPatchLabel.svelte
+++ src/routes/submit/CurrentPatchLabel.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import InfoTooltip from '$lib/Components/InfoTooltip.svelte';
 	import { currentPatchLabel } from '$lib/constants/patchCodes';
 </script>
 
 <div class="label">
-	<span class="label-text text-base font-semibold">Current Patch</span>
+	<span class="label-text text-base font-semibold"
+		>Current Patch <InfoTooltip
+			class="ml-1"
+			tip="If you need a different patch, please let us know in the run notes or contact us."
+		/></span
+	>
 </div>
 <input
-	class="input-bordered input bg-neutral"
+	class="input input-bordered bg-neutral"
 	type="text"
 	required
 	readonly
