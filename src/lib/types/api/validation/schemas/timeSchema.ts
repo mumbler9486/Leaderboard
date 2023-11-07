d78f6dc03c6a5a038a22a85afaef268e8fe98c24
mumbler9486 <mumbler9486@gmail.com>
2023-11-07 21:42:32 +0900

    ♻️ Refactor time validation to use create error message

--- src/lib/types/api/validation/schemas/timeSchema.ts
+++ src/lib/types/api/validation/schemas/timeSchema.ts
@@ -27,30 +27,33 @@ export const yupRunTime = (maxSeconds: number = 21599) => {
 export const yupRunTimeMapped = (validTimesMap: Record<string, number>) => {
 	return runTimeSchema.test(
 		'max_run_time',
-		(ctx) => {
+		(ctx) => `Invalid time validation configuration.`,
+		(runTime, ctx) => {
 			const validTime = validTimesMap[ctx.parent.category] ?? [];
 			if (validTime === undefined || validTime === null) {
-				return `Invalid time validation configuration.`;
+				return ctx.createError({
+					message: `Invalid time validation configuration.`,
+				});
 			}
 
 			const maxSeconds = validTime;
+
 			const hours = Math.floor(maxSeconds / 3600);
 			const minutes = Math.floor((maxSeconds % 3600) / 60);
 			const seconds = (maxSeconds % 3600) % 60;
 			const maxTimeLabel = `${hours}:${minutes.toString().padStart(2, '0')}:${seconds
 				.toString()
 				.padStart(2, '0')}`;
-			return `Run time cannot be more than ${maxTimeLabel}`;
-		},
-		(runTime, ctx) => {
-			const validTime = validTimesMap[ctx.parent.category] ?? [];
-			if (validTime === undefined || validTime === null) {
-				return false;
+			const runSeconds = runTime.hours * 3600 + runTime.minutes * 60 + runTime.seconds;
+
+			const isValid = runSeconds <= maxSeconds;
+			if (!isValid) {
+				return ctx.createError({
+					message: `Run time cannot be more than ${maxTimeLabel}`,
+				});
 			}
 
-			const maxSeconds = validTime;
-			const runSeconds = runTime.hours * 3600 + runTime.minutes * 60 + runTime.seconds;
-			return runSeconds <= maxSeconds;
+			return isValid;
 		}
 	);
 };
