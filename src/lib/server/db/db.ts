68cb2e636af4851554b5316179aecd6c2bfe076f
mumbler9486 <mumbler9486@gmail.com>
2024-01-08 18:50:09 +0900

    🔧 Add default to server port config

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -4,11 +4,14 @@ import sql from 'mssql';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+const serverPort =
+	!process.env.DB_PORT || process.env.DB_PORT === '' ? 1433 : parseInt(process.env.DB_PORT);
+
 const dbConfig: config = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	server: process.env.DB_SERVER ?? '',
-	port: parseInt(process.env.DB_PORT),
+	port: serverPort,
 	database: process.env.DB_NAME,
 	options: {
 		encrypt: true, // For Azure
