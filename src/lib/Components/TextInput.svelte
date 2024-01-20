513b19e610aea20a762f959d6c93ded4dd850009
mumbler9486 <mumbler9486@gmail.com>
2024-01-20 19:49:25 +0900

    ♻️ Refactor information input to use text input component

--- src/lib/Components/TextInput.svelte
+++ src/lib/Components/TextInput.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	export let value: string | undefined;
 	export let maxlength: number;
+	export let required: boolean = false;
 	export let placeholder: string = '';
 	export let prompt: string | undefined = undefined;
 	export let error: string | undefined = undefined;
+	export let alt: string | undefined = undefined;
 	export let primary: boolean = false;
 </script>
 
-<label class="form-control w-full max-w-xs">
+<label class="form-control w-full">
 	{#if !!prompt && prompt.length > 0}
 		<div class="label">
 			<span class="label-text">{prompt}</span>
@@ -15,8 +17,9 @@
 	{/if}
 	<input
 		type="text"
-		class="input input-bordered w-full max-w-xs"
+		class="input input-bordered w-full"
 		class:input-primary={primary}
+		{required}
 		{maxlength}
 		{placeholder}
 		bind:value
@@ -25,6 +28,11 @@
 	{#if !!error && error.length > 0}
 		<div class="label">
 			<span class="label-text-alt text-error">{error}</span>
+		</div>
+	{/if}
+	{#if !!alt && alt.length > 0}
+		<div class="label">
+			<span class="label-text-alt">{alt}</span>
 		</div>
 	{/if}
 </label>
