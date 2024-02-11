30be11786aed98d35923d630a833b5ce44918ec7
mumbler9486 <mumbler9486@gmail.com>
2024-02-11 11:40:14 +0900

    💬 Add back old news that was removed. Refactor old news

--- src/routes/info/news/AllNews.svelte
+++ src/routes/info/news/AllNews.svelte
@@ -99,3 +99,99 @@
 		site operations but as usual if there are issues please contact us on <DiscordContact />.
 	</NewsSection>
 </HomeNews>
+<HomeNews publicationDateIso="2023-06-14T00:00:00.000Z" title="Leaderboard 3.2 Update">
+	<NewsSection sectionTitle="⏫ Patch Bump for Level 75 Release">
+		All runs submitted from this point on will be considered part of the level 75 release patch.
+		This patch brings about skill re-balances from June 7th's update, Rarity 9⭐ weapons, Dark Falz
+		Aegis R3, and of course the level cap increase to 75.
+	</NewsSection>
+	<NewsSection sectionTitle="⚠ New Time Submission Rules">
+		<div class="[&>p]:py-2">
+			<p>
+				It has come to our attention that we need to have more consistent time measuring with
+				regards to how times are submitted on our leaderboard. The new rule now requires the
+				"remaining time" to be submitted instead of the usual (Time Limit) - (Time Remaining) =
+				Submission Time. This is because if a timer shows 19:59/20:00, technically a whole second
+				has not passed until the timer updates with a time of 19:58/20:00.
+			</p>
+			<p>
+				The UI has been adjusted to reflect this so please take care. All submission categories now
+				use this remaining time input system, however future quest content may utilize a different
+				system so it is subject to change.
+			</p>
+			<p>
+				We want to make everyone's submissions more accurate and consistent so we also went through
+				all Purple Trigger runs manually and issued a new time. In all cases everyone's time has
+				either stayed the same or improved by 1 second by the new rule. Dark Falz Aegis (solo and
+				party) and the Duel runs have not been affected yet and are still under consideration. If
+				you have any questions or concerns contact us on our Discord.
+			</p>
+		</div>
+	</NewsSection>
+	<NewsSection sectionTitle="💄 New Submission Landing Page">
+		The <a href="/submit">Submit a Run</a> page has got a new makeover with a new landing page. This
+		will make it easier for us to implement quest specific logic and have more leaderboard categories
+		in the future.
+	</NewsSection>
+</HomeNews>
+<HomeNews publicationDateIso="2023-04-18T00:00:00.000Z" title="Leaderboard 3.1 Update">
+	<NewsSection sectionTitle="Indomitable Duel Runs now Accepting Submissions">
+		Duel runs can now be submitted to the leaderboard. Please take note if you have used the special
+		Duel augments or not.
+	</NewsSection>
+	<NewsSection sectionTitle="About Special Duel Augments">
+		Currently it will be either you affixed an augment on any equipment in the run or you did not.
+		Since we don't know the impact on the runs yet this may be subject to change. We will reach out
+		to the early submitters where possible to improve their submissions if there is a change needed.
+	</NewsSection>
+	<NewsSection>
+		Any questions or issues please contact us on our <DiscordContact />
+	</NewsSection>
+</HomeNews>
+<HomeNews publicationDateIso="2023-03-03T00:00:00.000Z" title="Leaderboard Mega Update">
+	A very small, yet very large update has been released.<br /><br />
+	<NewsSection sectionTitle="Full Site Rewrite">
+		The website has been fully rewritten.<br /><br />This has lead to some visual changes, and will
+		make implementing future feature updates easier.
+	</NewsSection>
+	<NewsSection sectionTitle="Youtube Embeds">
+		Enabling youtube cookies will replace video links on run info panels with embeds!<br />
+		Please note that this only applies to solo runs.
+	</NewsSection>
+	<NewsSection sectionTitle="Duo and Party Runs">Duo and party runs have returned!</NewsSection>
+	<NewsSection sectionTitle="Dark Falz Aegis Leaderboards">
+		Leaderboards for solo, duo, and full MPA have been added for Dark Falz Aegis!
+	</NewsSection>
+	<NewsSection sectionTitle="Player Descriptions">
+		Player description editing has been enabled.
+	</NewsSection>
+	<NewsSection sectionTitle="Player Trophies">
+		Trophies for each class have been added, in bronze, silver, gold, and rainbow types.<br />
+		The first three trophy ranks will be given as a reward for participation in the below feature.<br
+		/>
+		Rainbow trophies will be given out only for specific users, with a more specific criteria still being
+		decided.<br />
+		At this point in time, only guide authors have rainbow tier trophies.
+	</NewsSection>
+	<NewsSection sectionTitle="Meteorn Records">
+		Functionality for 'Meteorn Records', or unofficial ARKs Records have been implemented.<br />
+		Our test run is Dark Falz Aegis MPA runs.<br />
+		Every player in a party with a bronze, silver, or gold ranking will be given a trophy for their party
+		rank and class!
+	</NewsSection>
+	<NewsSection sectionTitle="Backend Alliance Support">
+		More information will be released later, but Alliances are technically fully functional, but
+		temporarily unavailable!
+	</NewsSection>
+	<NewsSection sectionTitle="Backend Avatar Support">
+		Player avatars have been implemented, but remain disabled for the time being!
+	</NewsSection>
+	<NewsSection sectionTitle="Backend Twitch Support">
+		Backend work has been done to enable Twitch embeds and videos in the future.<br />
+		This will include the ability to display your Twitch stream on your profile if you wish!<br />
+		Make sure to turn on Twitch cookies if you want to see this feature when it's implemented in full!
+	</NewsSection>
+	<NewsSection sectionTitle="Discord Widget">
+		If you enable Discord cookies, a Discord widget will appear on the home page.
+	</NewsSection>
+</HomeNews>
