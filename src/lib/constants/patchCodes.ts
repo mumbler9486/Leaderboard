ca2a800d3386df6561d2852f2735e6dba0e470fe
mumbler9486 <mumbler9486@gmail.com>
2024-06-01 18:30:22 +0900

    💬 Actually bump patch level 90

--- src/lib/constants/patchCodes.ts
+++ src/lib/constants/patchCodes.ts
@@ -1,5 +1,5 @@
 // The current patch code for all submissions
-export const CurrentSubmissionPatchCode = 'apr2024xover';
+export const CurrentSubmissionPatchCode = 'level90r';
 
 // Keep patch codes under 30 chars
 export const patchCodeLabelMap = {
