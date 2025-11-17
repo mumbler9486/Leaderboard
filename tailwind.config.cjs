40e51786974eaf5a4a44add10da5677f625bf56a
mumbler9486 <mumbler9486@gmail.com>
2025-11-18 07:32:42 +0900

    🚚 Move daisyui theme to app.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,24 +2,7 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-	daisyui: {
-		themes: [
-			{
-				ngsTheme: {
-					primary: '#3CB1F5',
-					secondary: '#6C757D',
-					accent: '#F8F9FA',
-					neutral: '#0C274B',
-					'base-100': '#212529',
-					info: '#4367E1',
-					success: '#B1F53C',
-					warning: '#EBAF3B',
-					error: '#E14343'
-				}
-			}
-		]
-	},
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui')],
 };
