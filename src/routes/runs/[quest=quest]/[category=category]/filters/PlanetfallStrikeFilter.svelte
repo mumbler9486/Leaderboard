61573ccca1e4c9f6ac96b94bc066d0c93fdbff95
mumbler9486 <mumbler9486@gmail.com>
2025-12-02 23:10:03 +0900

    🐛 Fix filter on planet fall strike not visible

--- src/routes/runs/[quest=quest]/[category=category]/filters/PlanetfallStrikeFilter.svelte
+++ src/routes/runs/[quest=quest]/[category=category]/filters/PlanetfallStrikeFilter.svelte
@@ -3,12 +3,12 @@
 	import { planetfallStageBossNameMappings } from '$lib/leaderboard/boards/duelPlanetfall';
 	import { runFilters } from '../../../runFilter';
 
-	const augmentFilterOptions = [
-		Object.values(planetfallStageBossNameMappings).map((bossName, i) => ({
+	const augmentFilterOptions = Object.values(planetfallStageBossNameMappings).map(
+		(bossName, i) => ({
 			label: `${i + 1}: ${bossName}`,
-			value: i + 1,
-		})),
-	];
+			value: `${i + 1}`,
+		})
+	);
 </script>
 
 <RadioOptions
